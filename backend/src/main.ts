171a745f55880ad0529f74863a7b1394ee9ab906
RasWayne <juliengong1@gmail.com>
2026-03-10 23:25:22 -0400

    fix: production Dockerfile and Cloud Run port configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
 
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
@@ -16,7 +14,7 @@ async function bootstrap(): Promise<void> {
     }),
   );
 
-  const port = configService.get<number>('app.port', 4000);
+  const port = process.env.PORT || 4000;
   await app.listen(port);
 }
 
